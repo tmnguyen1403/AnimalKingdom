778523e877ca330663eabb4806b7db28ade10112
Minh Nguyen <tmnguyen1403@gmail.com>
2020-12-11 02:22:02 -0600

    Use TabBarController

---
+++ AnimalKingdom/Controllers/MyTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MyTabBarController.swift
+//  AnimalKingdom
+//
+//  Created by Tom Riddle on 12/11/20.
+//
+
+import UIKit
+
+class MyTabBarController: UITabBarController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
